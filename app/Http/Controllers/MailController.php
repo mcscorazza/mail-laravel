70da4c534c4108aba8dfd3cd648f813e28312dba
mcscorazza <mcs.corazza@gmail.com>
2024-01-10 14:43:30 -0300

    blade

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 class MailController extends Controller
 {
     public function mail() {
-        $htmlContent = '<!doctype html><html lang="pt-BR"><body style="padding:0;margin:0;width:100%;min-height:100vh;background:#f6f6f6;"><div style="width:50%;min-height:100vh;background:red;color:#ffffff;">TESTE</div></body></html>';
+        $htmlContent = view('emails.welcome')->render();
 
         Mail::send([], [], function ($message) use ($htmlContent){
             $message->to('marcos.corazza@gbmx.com.br');
