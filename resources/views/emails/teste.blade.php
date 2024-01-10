b9a6961c3d18bdf1cdf9767ac44240bf16a37da0
Marcos Corazza <mcs.corazza@gmail.com>
2024-01-10 16:09:02 -0300

    blade1

--- resources/views/emails/teste.blade.php
+++ resources/views/emails/teste.blade.php
@@ -3,14 +3,13 @@
 <head>
     <title>Welcome Email</title>
 </head>
-<body style="width:100%;font-family:'Segoe UI',Tahoma,Geneva,Verdana,sans-serif;margin:0;">
-
+<body style="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif">
     <div style="text-align: center;width:100%;">
-        <h1>Welcome to our platform!</h1>
-        <img src="https://gbmx.com.br/wp-content/themes/gbmx/img/logo_gbmx_footer.png" alt="GBMX">
-        <p>Thank you for joining us.</p>
+        <h1>GB DIGITAL REPORT</h1>
+        <svg width="458" height="106" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" overflow="hidden"><g transform="translate(-3355 -991)"><path d="M3355 1043 3381.91 1043 3420 1004.29 3406.92 991 3355 1043Z" fill="#00B082" fill-rule="evenodd"/><path d="M3355 1045 3382.21 1045 3406 1069.26 3406 1097 3355 1045Z" fill="#00B082" fill-rule="evenodd"/><path d="M3408 1097 3408 1069.45 3432.5 1045 3460 1045 3408 1097Z" fill="#00B082" fill-rule="evenodd"/><path d="M3394 1044.35 3426 1012 3426 1039.19 3407.4 1058 3394 1044.35Z" fill="#00B082" fill-rule="evenodd"/><path d="M3428 1012 3428 1038.59 3432.48 1043 3460 1043 3428 1012Z" fill="#00B082" fill-rule="evenodd"/><text fill="#00B082" font-family="Calibri,Calibri_MSFontService,sans-serif" font-weight="400" font-size="32" transform="matrix(1 0 0 1 3469.53 1047)">GREENBRIER MAXION</text><text fill="#00B082" font-family="Calibri,Calibri_MSFontService,sans-serif" font-weight="400" font-size="20" transform="matrix(1 0 0 1 3469.53 1068)">AN AMSTED RAIL GLOBAL PARTNER</text></g></svg>
+        <p>Relatório XPTO</p>
         <div style="margin:0 auto;text-align:center;width:500px;line-height:100px;border:1px solid black;">
-            CAIXA TESTE
+            Teste de Dados tabulados dentro de uma caixa
         </div>
     </div>
 </body>
