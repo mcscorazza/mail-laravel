c253cafe436032ba6d71f7bb6b25eb606a616208
mcscorazza <mcs.corazza@gmail.com>
2024-01-10 12:06:09 -0300

    init

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Mail\Message;
+use Illuminate\Http\Request;
+
+class MailController extends Controller
+{
+    public function mail() {
+        $htmlContent = '<p>This is the HTML content of the email</p>';
+
+        Mail::send([], [], function ($message) use ($htmlContent){
+            $message->to('marcos.corazza@gbmx.com.br');
+            $message->subject('GB Digital Report');
+            $message->html($htmlContent);
+        });
+        return "Email enviado com sucesso!";
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MailController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+$router->get('/', [MailController::class, 'mail']);
